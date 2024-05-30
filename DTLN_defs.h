fd28477c6f99b7c766fe45183c5ad96b737afaab
William RUAN <56035086+William1617@users.noreply.github.com>
2024-05-30 16:40:09 +0800

    Update DTLN_defs.h

--- DTLN_defs.h
+++ DTLN_defs.h
@@ -27,6 +27,8 @@
 #define DTLN_STATE_SIZE  (512)
 #define DTLN_UNIT_SIZE (128)
 #define FFT_OUT_SIZE    (DTLN_BLOCK_LEN / 2 + 1)
+#define MODEL_INPUT_NUM (5)
+#define MODEL_OUTPUT_NUM (5)
 
 
 struct trg_engine {
@@ -38,4 +40,18 @@ struct trg_engine {
     
    
 };
+struct trg_engine_zerocopy {
+    float in_buffer[DTLN_BLOCK_LEN] = { 0 };
+    float out_buffer[DTLN_BLOCK_LEN] = { 0 };
+  
+    std::vector<std::vector<float>> statesa;
+    std::vector<std::vector<float>> statesb;
+    
+    rknn_tensor_mem*  m_input_details_a[MODEL_INPUT_NUM];
+    rknn_tensor_mem*  m_input_details_b[MODEL_INPUT_NUM];
+    rknn_tensor_mem*  m_output_details_a[MODEL_OUTPUT_NUM];
+    rknn_tensor_mem*  m_output_details_b[MODEL_OUTPUT_NUM];
+
+};
+
 
